86c82d5f233616a4d1efc6209b1a38efd867e468
Alexander K <wldwldcrd@gmail.com>
2020-06-13 22:05:05 +0300

    correct nginx.conf

--- hello.py
+++ hello.py
@@ -1,5 +1,17 @@
-to their definitions or references in the same repository. Learn more
-def app(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),
-                  encoding="utf8")]
+from urlparse import parse_qs
+
+
+def application(environ, start_response):
+    query = parse_qs(environ['QUERY_STRING'], keep_blank_values=True)
+    body = []
+    for key, value in query.items():
+        for item in value:
+            body.append(key + "=" + item + "\r\n")
+
+    status = '200 OK'
+    headers = [
+        ('Content-Type', 'text/plain')
+    ]
+
+    start_response(status, headers)
+    return body
