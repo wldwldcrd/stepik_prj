8fdc045f59575f08ea8ce98af93a4d5922184ae9
Alexander K <wldwldcrd@gmail.com>
2020-06-13 22:11:10 +0300

    fix init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,13 @@
-﻿sudo /etc/init.d/nginx start
+﻿#!/bin/bash
+
+if [ -f /etc/nginx/sites-enabled/default ]; then
+  sudo rm /etc/nginx/sites-enabled/default
+fi
+
+# Nginx
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+
+# Gunicorn
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
