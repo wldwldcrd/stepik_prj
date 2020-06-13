58c0b7329eb185e0b481f24829e0374fb5d6dbdb
Alexander K <wldwldcrd@gmail.com>
2020-06-13 23:18:57 +0300

    Revert "Add comments"
    This reverts commit cb1e288a07ac422a70bc08e49570d17ec441dc74.

--- init.sh
+++ init.sh
@@ -4,10 +4,10 @@ if [ -f /etc/nginx/sites-enabled/default ]; then
   sudo rm /etc/nginx/sites-enabled/default
 fi
 
-# Nginx подключаем конфиг с помощью символической ссылки
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf # загрузка нового конфига
-sudo /etc/init.d/nginx restart # перезапуск Nginx для применения нового конфига
+# Nginx
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
 
 # Gunicorn
-sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test # загрузка нового конфига
-sudo /etc/init.d/gunicorn restart # перезапуск gunicorn для применения нового конфига
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
